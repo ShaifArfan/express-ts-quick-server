f6c23731aadd0b6914a20e4fa97143fb4d5c7166
Arfan <shaifarfan08@gmail.com>
2023-01-10 05:08:25 +0600

    adding stuff 📥

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
 import express from "express";
+import allRoutes from "./routes";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
+
+// middleware
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/api", allRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
