a817194427d96066b738deb3a6e37fe753605efd
Arfan <shaifarfan08@gmail.com>
2023-01-13 21:19:24 +0600

    catching port already used issue

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,54 @@ import express from "express";
 import allRoutes from "./routes";
 import morgan from "morgan";
 import cors from "cors";
+import chalk from "chalk";
 
-const app = express();
+// dotenv
+import * as dotenv from "dotenv";
+dotenv.config();
+
+let PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+
+const server = express();
 
 // middleware
-app.use(morgan("tiny"));
-app.use(cors());
-app.use(express.json());
+server.use(morgan("tiny"));
+server.use(cors());
+server.use(express.json());
 
 // routes
-app.use("/api", allRoutes);
+server.use("/api", allRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+server
+  .listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+  })
+  .on("error", (error: any) => {
+    if (error.code === "EADDRINUSE") {
+      console.log(chalk.red(`Port ${PORT} is already in use`));
+      if (process.platform === "win32") {
+        console.log(
+          chalk.red(
+            `Try ➤ "SET PORT=${
+              PORT + 1
+            } && npm run dev" to run the server on a different port`
+          )
+        );
+      } else if (
+        process.platform === "linux" ||
+        process.platform === "darwin"
+      ) {
+        console.log(
+          chalk.yellow(
+            `Try ➤ "PORT=${
+              PORT + 1
+            } npm run dev" to run the server on a different port`
+          )
+        );
+      } else {
+        console.log("Try running the server on a different port");
+      }
+    }
+    console.log("Exiting process...");
+    process.exit(1);
+  });
